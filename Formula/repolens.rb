c19a9e3aaa0553c4bd5abe646f24cdecbd522078
Doug.Skinner <Doug.Skinner@parexel.com>
2026-05-07 15:42:20 -0400

    Initial formula (placeholder until first release)

---
+++ Formula/repolens.rb
@@ -0,0 +1,32 @@
+class Repolens < Formula
+  desc "TUI dashboard for your GitHub repositories"
+  homepage "https://github.com/doug-skinner/repolens"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/doug-skinner/repolens/releases/download/v#{version}/repolens-darwin-arm64"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/doug-skinner/repolens/releases/download/v#{version}/repolens-darwin-x64"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/doug-skinner/repolens/releases/download/v#{version}/repolens-linux-arm64"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/doug-skinner/repolens/releases/download/v#{version}/repolens-linux-x64"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install Dir["repolens-*"].first => "repolens"
+  end
+end
